86cddfef6fdefa86d4f3764cbc2a16d2172fd9f5
Khadras Wellun <116345441+KhadrasWellun@users.noreply.github.com>
2023-06-10 21:21:38 +0300

    Add files via upload

--- particle_life/src/main.cpp
+++ particle_life/src/main.cpp
@@ -7,9 +7,8 @@ int main( ){
 	//ofSetupOpenGL(1920,1080,OF_WINDOW);
 	ofGLWindowSettings s;
 	//s.setGLVersion(4, 3);
-	//s.setSize(1920, 1080);
 	s.setSize(1920, 1080);
-	s.setPosition(glm::vec2(0, 0));
+	//s.setPosition(glm::vec2(0, 0));
 	ofCreateWindow(s);
 
 	ofRunApp(new ofApp());
